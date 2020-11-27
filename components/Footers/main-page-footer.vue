57ed858f1f294d07bbebd1557988db24d252c88e
Ahmad Hashemi <ahmadhashemipour@outlook.com>
2020-11-27 18:42:41 +0330

    WIP:create footer components

---
+++ components/Footers/main-page-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this is main page footer
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" >
+
+</style>
---
+++ components/Footers/page-footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    this is page footer
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
